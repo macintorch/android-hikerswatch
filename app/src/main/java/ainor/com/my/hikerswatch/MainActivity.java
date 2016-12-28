cd52407596eb5c9d57ef9d67b9fc05615124bef5
Ainor Syahrizal <macintorch@gmail.com>
2016-12-28 17:41:12 +0800

    add user request permission

--- app/src/main/java/ainor/com/my/hikerswatch/MainActivity.java
+++ app/src/main/java/ainor/com/my/hikerswatch/MainActivity.java
@@ -7,6 +7,7 @@ import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Build;
+import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
@@ -17,6 +18,24 @@ public class MainActivity extends AppCompatActivity {
 
     LocationManager mLocationManager;
     LocationListener mLocationListener;
+
+
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        if (grantResults.length >0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+            startListening();
+        }
+    }
+
+
+    public void startListening() {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            mLocationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
+        }
+    }
 
     public void updateLocationInfo(Location location) {
         Log.i("LocationInfo",location.toString());
@@ -53,7 +72,7 @@ public class MainActivity extends AppCompatActivity {
         };
 
         if (Build.VERSION.SDK_INT<23) {
-            mLocationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,0,0, mLocationListener);
+            startListening();
         } else {
             if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED){
                 ActivityCompat.requestPermissions(this, new String[] {Manifest.permission.ACCESS_FINE_LOCATION}, 1);
@@ -65,8 +84,6 @@ public class MainActivity extends AppCompatActivity {
                 if (location != null) {
                     updateLocationInfo(location);
                 }
-
-
 
             }
 
